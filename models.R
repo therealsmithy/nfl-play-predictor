49367b772599bfee5c3f10ff430545937cb88f2f
Liam Smith <91901893+therealsmithy@users.noreply.github.com>
2025-02-06 20:13:40 -0500

    New models added

--- models.R
+++ models.R
@@ -15,7 +15,7 @@ library(fastDummies)
 library(forecast)
 
 # Load in data
-load('G:/My Drive/MSBA/ML/Final Project/nfl-play-predictor/data/model_df.rda')
+load('G:/My Drive/MSBA/Sem1/ML/Final Project/nfl-play-predictor/data/model_df.rda')
 
 # Split data
 total_obs <- nrow(model_df)
@@ -827,9 +827,399 @@ shap_long <- shap.prep(shap = shap_result,
 shap_rp_move <- plot.shap.summary(data_long = shap_long)
 ggsave(shap_rp_move, file = "G:/My Drive/MSBA/ML/Final Project/nfl-play-predictor/shap_rp_move.jpeg", dpi = 600)
 
+# SIMPLE TREE - WITH MOVEMENT AND S/A ADDED
+train_data_rp_move_sa <- train_data %>% 
+                      select(-c(playDescription, expectedPointsAdded))
+tree5 <- rpart(run_pass ~ ., 
+               data = train_data_rp_move_sa,
+               control = rpart.control(minsplit = 100, minbucket = 30, cp = 0.002))
+fancyRpartPlot(tree5, cex = 0.5)
+tree5_preds <- predict(tree5, test_data, type = 'vector')
+tree5_preds <- ifelse(tree5_preds > 1, 'run', 'pass')
+t11 <- table(tree5_preds, test_data$run_pass)
+confusionMatrix(t11)
 
+# RANDOM FOREST - WITH MOVEMENT AND S/A ADDED
+train_data_bag <- train_data_rp_move_sa %>% 
+                    na.omit()
+bag5 <- randomForest(run_pass ~ .,
+                     data = train_data_bag,
+                     mtry = 25,
+                     ntree = 500)
+bag5_preds <- predict(bag5, test_data, type = 'class')
+t12 <- table(bag5_preds, test_data$run_pass)
+confusionMatrix(t12)
+# Plot error to see if we should increase the number of trees
+oob_error <- bag5$err.rate[, 1]
+plot_dat <- cbind.data.frame(rep(1:length(oob_error)), oob_error)
+names(plot_dat) <- c('trees', 'oob_error')
+ggplot(plot_dat, aes(x = trees, y = oob_error)) +
+  geom_line(alpha = 0.5, color = 'green') +
+  labs(title = 'OOB Error Rate vs Number of Trees',
+       x = 'Number of Trees',
+       y = 'OOB Error Rate') +
+  theme_minimal()
+# Parameter tuning
+mtry <- c(20, 21, 22, 23, 24, 25)
+nodesize <- c(1, 10, 25, 50, 100, 200, 500, 1000)
+params <- expand.grid(mtry, nodesize)
+names(params) <- c('mtry', 'nodesize')
+res_vec <- rep(NA, nrow(params))
+for(i in 1:nrow(params)){
+  print(i)
+  set.seed(102701)
+  mod <- randomForest(run_pass ~ .,
+                      data = train_data_bag,
+                      mtry = params$mtry[i],
+                      ntree = 500,
+                      nodesize = params$nodesize[i])
+  res_vec[i] <- 1 - mod$err.rate[nrow(mod$err.rate), 1]
+}
+summary(res_vec)
+# Visualize best parameters
+res_db <- cbind.data.frame(params, res_vec)
+names(res_db)[3] <- 'oob_accuracy'
+res_db$trees <- as.factor(res_db$mtry)
+res_db$nodesize <- as.factor(res_db$nodesize)
+ggplot(res_db, aes(x = trees, y = nodesize, fill = oob_accuracy)) +
+  geom_tile() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red',
+                       midpoint = mean(res_db$oob_accuracy), space = 'Lab',
+                       na.value = 'gray', guide = 'colorbar', aesthetics = 'fill') +
+  labs(title = 'OOB Accuracy by Number of Trees and Nodesize',
+       x = 'Number of Trees',
+       y = 'Nodesize',
+       fill = 'OOB Accuracy') +
+  theme_minimal()
+res_db[which.max(res_db$oob_accuracy), ]
+# 25 trees with 50 nodesize does the best
+bag6 <- randomForest(run_pass ~ .,
+                     data = train_data_bag,
+                     mtry = 25,
+                     ntree = 50,
+                     nodesize = 50)
+bag6_preds <- predict(bag6, test_data, type = 'class')
+t13 <- table(bag6_preds, test_data$run_pass)
+confusionMatrix(t13)
 
-
+# XGBOOST - WITH MOVEMENT AND S/A ADDED
+# Set up dummies
+train_data_xgb <- train_data_rp_move_sa %>% 
+                    dummy_cols(select_columns = c("offenseFormation", "receiverAlignment"))
+test_data_xgb <- test_data %>%
+                    dummy_cols(select_columns = c("offenseFormation", "receiverAlignment"))
+# Set up DMatrix
+dtrain_xgb_rp_move_sa <- xgb.DMatrix(data = as.matrix(train_data_xgb[, c(2:3, 6:97)]),
+                                     label = as.numeric(train_data_xgb$run_pass) - 1)
+dtest_xgb_rp_move_sa <- xgb.DMatrix(data = as.matrix(test_data_xgb[, c(4:5, 8:99)]),
+                                    label = as.numeric(test_data_xgb$run_pass) - 1)
+# Train and Predict
+xgb7 <- xgboost(data = dtrain_xgb_rp_move_sa,
+                nrounds = 100,
+                verbose = 1,
+                print_every_n = 20,
+                objective = 'binary:logistic',
+                eval_metric = 'auc',
+                eval_metric = 'error')
+xgb7_preds <- predict(xgb7, dtest_xgb_rp_move_sa)
+pred_dat <- cbind.data.frame(xgb7_preds, test_data_xgb$run_pass)
+boost_pred_class <- rep('pass', length(xgb7_preds))
+boost_pred_class[xgb7_preds > 0.5] <- 'run'
+t14 <- table(boost_pred_class, test_data_xgb$run_pass)
+confusionMatrix(t14)
+# Model Tuning
+bst <- xgboost(data = dtrain_xgb_rp_move_sa,
+               nfold = 5,
+               eta = 0.1,
+               nrounds = 2000,
+               early_stopping_rounds = 50,
+               verbose = 1,
+               nthread = 1,
+               print_every_n = 20,
+               objective = 'binary:logistic',
+               eval_metric = 'auc',
+               eval_metric = 'error')
+# Best iteration is 981, will train at 1200
+# Tuning max depth and min child weight
+cv_params <- expand.grid(max_depth_vals, min_child_weight_vals)
+names(cv_params) <- c('max_depth', 'min_child_weight')
+auc_vec <- error_vec <- rep(NA, nrow(cv_params))
+for(i in 1:nrow(cv_params)){
+  print(i)
+  set.seed(102701)
+  bst_tune <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                   nfold = 5,
+                   eta = 0.1,
+                   nrounds = 1200,
+                   early_stopping_rounds = 20,
+                   verbose = 1,
+                   nthread = 1,
+                   print_every_n = 20,
+                   objective = 'binary:logistic',
+                   eval_metric = 'auc',
+                   eval_metric = 'error',
+                   max_depth = cv_params$max_depth[i],
+                   min_child_weight = cv_params$min_child_weight[i])
+  auc_vec[i] <- max(bst_tune$evaluation_log$test_auc_mean[bst_tune$best_ntreelimit])
+  error_vec[i] <- min(bst_tune$evaluation_log$test_error_mean[bst_tune$best_ntreelimit])
+}
+res_db <- cbind.data.frame(cv_params, auc_vec, error_vec)
+names(res_db)[3:4] <- c('auc', 'error')
+res_db$max_depth <- as.factor(res_db$max_depth)
+res_db$min_child_weight <- as.factor(res_db$min_child_weight)
+# AUC heatmap
+ggplot(res_db, aes(x = max_depth, y = min_child_weight, fill = auc)) +
+  geom_tile() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red',
+                       midpoint = mean(res_db$auc), space = 'Lab',
+                       na.value = 'gray', guide = 'colorbar', aesthetics = 'fill') +
+  labs(title = 'AUC by Max Depth and Min Child Weight',
+       x = 'Max Depth',
+       y = 'Min Child Weight',
+       fill = 'AUC') +
+  theme_minimal()
+# Error heatmap
+ggplot(res_db, aes(x = max_depth, y = min_child_weight, fill = error)) +
+  geom_tile() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red',
+                       midpoint = mean(res_db$error), space = 'Lab',
+                       na.value = 'gray', guide = 'colorbar', aesthetics = 'fill') +
+  labs(title = 'Error by Max Depth and Min Child Weight',
+       x = 'Max Depth',
+       y = 'Min Child Weight',
+       fill = 'Error') +
+  theme_minimal()
+# Max Depth of 7 and min child weight of 3
+# Tuning gamma
+auc_vec <- error_vec <- rep(NA, length(gamma_vals))
+for(i in 1:length(gamma_vals)){
+  print(i)
+  set.seed(102701)
+  bst_tune <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                   nfold = 5,
+                   eta = 0.1,
+                   nrounds = 1200,
+                   early_stopping_rounds = 20,
+                   verbose = 1,
+                   nthread = 1,
+                   print_every_n = 20,
+                   objective = 'binary:logistic',
+                   eval_metric = 'auc',
+                   eval_metric = 'error',
+                   max_depth = 7,
+                   min_child_weight = 3,
+                   gamma = gamma_vals[i])
+  auc_vec[i] <- max(bst_tune$evaluation_log$test_auc_mean[bst_tune$best_ntreelimit])
+  error_vec[i] <- min(bst_tune$evaluation_log$test_error_mean[bst_tune$best_ntreelimit])
+}
+cbind.data.frame(gamma_vals, auc_vec, error_vec)
+# THe best gamma is 5
+# Now retune the initial model with the things we know now
+xgb8 <- xgboost(data = dtrain_xgb_rp_move_sa,
+                nfold = 5,
+                eta = 0.1,
+                nrounds = 1200,
+                early_stopping_rounds = 50,
+                verbose = 1,
+                nthread = 1,
+                print_every_n = 20,
+                objective = 'binary:logistic',
+                eval_metric = 'auc',
+                eval_metric = 'error',
+                max_depth = 7,
+                min_child_weight = 3,
+                gamma = 5)
+# Next tune by subsample and colsample by tree
+cv_params <- expand.grid(subsample_vals, colsample_bytree_vals)
+names(cv_params) <- c('subsample', 'colsample_bytree')
+auc_vec <- error_vec <- rep(NA, nrow(cv_params))
+for(i in 1:nrow(cv_params)){
+  print(i)
+  set.seed(102701)
+  bst_tune <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                   nfold = 5,
+                   eta = 0.1,
+                   nrounds = 1200,
+                   early_stopping_rounds = 50,
+                   verbose = 1,
+                   nthread = 1,
+                   print_every_n = 20,
+                   objective = 'binary:logistic',
+                   eval_metric = 'auc',
+                   eval_metric = 'error',
+                   max_depth = 7,
+                   min_child_weight = 3,
+                   gamma = 5,
+                   subsample = cv_params$subsample[i],
+                   colsample_bytree = cv_params$colsample_bytree[i])
+  auc_vec[i] <- max(bst_tune$evaluation_log$test_auc_mean[bst_tune$best_ntreelimit])
+  error_vec[i] <- min(bst_tune$evaluation_log$test_error_mean[bst_tune$best_ntreelimit])
+}
+res_db <- cbind.data.frame(cv_params, auc_vec, error_vec)
+names(res_db)[3:4] <- c('auc', 'error')
+res_db$subsample <- as.factor(res_db$subsample)
+res_db$colsample_bytree <- as.factor(res_db$colsample_bytree)
+# AUC Heatmap
+ggplot(res_db, aes(x = subsample, y = colsample_bytree, fill = auc)) +
+  geom_tile() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red',
+                       midpoint = mean(res_db$auc), space = 'Lab',
+                       na.value = 'gray', guide = 'colorbar', aesthetics = 'fill') +
+  labs(title = 'AUC by Subsample and Colsample by Tree',
+       x = 'Subsample',
+       y = 'Colsample by Tree',
+       fill = 'AUC') +
+  theme_minimal()
+# Error Heatmap
+ggplot(res_db, aes(x = subsample, y = colsample_bytree, fill = error)) +
+  geom_tile() +
+  scale_fill_gradient2(low = 'blue', mid = 'white', high = 'red',
+                       midpoint = mean(res_db$error), space = 'Lab',
+                       na.value = 'gray', guide = 'colorbar', aesthetics = 'fill') +
+  labs(title = 'Error by Subsample and Colsample by Tree',
+       x = 'Subsample',
+       y = 'Colsample by Tree',
+       fill = 'Error') +
+  theme_minimal()
+# Colsample of 0.7 and subsample of 0.9 is best
+# Now just to check out different eta values
+bst_mod_1 <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                    nfold = 5,
+                    eta = 0.3,
+                    nrounds = 1200,
+                    early_stopping_rounds = 50,
+                    verbose = 1,
+                    nthread = 1,
+                    print_every_n = 20,
+                    objective = 'binary:logistic',
+                    eval_metric = 'auc',
+                    eval_metric = 'error',
+                    max_depth = 7,
+                    min_child_weight = 3,
+                    gamma = 5,
+                    subsample = 0.9,
+                    colsample_bytree = 0.7)
+bst_mod_2 <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                    nfold = 5,
+                    eta = 0.1,
+                    nrounds = 1200,
+                    early_stopping_rounds = 50,
+                    verbose = 1,
+                    nthread = 1,
+                    print_every_n = 20,
+                    objective = 'binary:logistic',
+                    eval_metric = 'auc',
+                    eval_metric = 'error',
+                    max_depth = 7,
+                    min_child_weight = 3,
+                    gamma = 5,
+                    subsample = 0.9,
+                    colsample_bytree = 0.7)
+bst_mod_3 <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                    nfold = 5,
+                    eta = 0.05,
+                    nrounds = 1200,
+                    early_stopping_rounds = 50,
+                    verbose = 1,
+                    nthread = 1,
+                    print_every_n = 20,
+                    objective = 'binary:logistic',
+                    eval_metric = 'auc',
+                    eval_metric = 'error',
+                    max_depth = 7,
+                    min_child_weight = 3,
+                    gamma = 5,
+                    subsample = 0.9,
+                    colsample_bytree = 0.7)
+bst_mod_4 <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                    nfold = 5,
+                    eta = 0.01,
+                    nrounds = 1200,
+                    early_stopping_rounds = 50,
+                    verbose = 1,
+                    nthread = 1,
+                    print_every_n = 20,
+                    objective = 'binary:logistic',
+                    eval_metric = 'auc',
+                    eval_metric = 'error',
+                    max_depth = 7,
+                    min_child_weight = 3,
+                    gamma = 5,
+                    subsample = 0.9,
+                    colsample_bytree = 0.7)
+bst_mod_5 <- xgb.cv(data = dtrain_xgb_rp_move_sa,
+                    nfold = 5,
+                    eta = 0.005,
+                    nrounds = 1200,
+                    early_stopping_rounds = 50,
+                    verbose = 1,
+                    nthread = 1,
+                    print_every_n = 20,
+                    objective = 'binary:logistic',
+                    eval_metric = 'auc',
+                    eval_metric = 'error',
+                    max_depth = 7,
+                    min_child_weight = 3,
+                    gamma = 5,
+                    subsample = 0.9,
+                    colsample_bytree = 0.7)
+# Now to plot and see
+pd1 <- cbind.data.frame(bst_mod_1$evaluation_log[, c('iter', 'test_error_mean')],
+                        rep('0.3', nrow(bst_mod_1$evaluation_log)))
+names(pd1)[3] <- 'eta'
+pd2 <- cbind.data.frame(bst_mod_2$evaluation_log[, c('iter', 'test_error_mean')],
+                        rep('0.1', nrow(bst_mod_2$evaluation_log)))
+names(pd2)[3] <- 'eta'
+pd3 <- cbind.data.frame(bst_mod_3$evaluation_log[, c('iter', 'test_error_mean')],
+                        rep('0.05', nrow(bst_mod_3$evaluation_log)))
+names(pd3)[3] <- 'eta'
+pd4 <- cbind.data.frame(bst_mod_4$evaluation_log[, c('iter', 'test_error_mean')],
+                        rep('0.01', nrow(bst_mod_4$evaluation_log)))
+names(pd4)[3] <- 'eta'
+pd5 <- cbind.data.frame(bst_mod_5$evaluation_log[, c('iter', 'test_error_mean')],
+                        rep('0.005', nrow(bst_mod_5$evaluation_log)))
+names(pd5)[3] <- 'eta'
+plot_dat <- rbind(pd1, pd2, pd3, pd4, pd5)
+ggplot(plot_dat, aes(x = iter, y = test_error_mean, color = eta)) +
+  geom_smooth(alpha = 0.5) +
+  labs(title = 'Error by Iteration and Eta',
+       x = 'Iteration',
+       y = 'Error',
+       color = 'Eta') +
+  theme_minimal()
+# 0.05 is best
+xgb8 <- xgboost(data = dtrain_xgb_rp_move_sa,
+                nfold = 5,
+                eta = 0.05,
+                nrounds = 1200,
+                early_stopping_rounds = 50,
+                verbose = 1,
+                nthread = 1,
+                print_every_n = 20,
+                objective = 'binary:logistic',
+                eval_metric = 'auc',
+                eval_metric = 'error',
+                max_depth = 7,
+                min_child_weight = 3,
+                gamma = 5,
+                subsample = 0.9,
+                colsample_bytree = 0.7)
+# Final Prediction for This One
+xgb8_preds <- predict(xgb8, dtest_xgb_rp_move_sa)
+pred_dat <- cbind.data.frame(xgb8_preds, test_data_xgb$run_pass)
+boost_pred_class <- rep('pass', length(xgb8_preds))
+boost_pred_class[xgb8_preds > 0.5] <- 'run'
+t15 <- table(boost_pred_class, test_data_xgb$run_pass)
+confusionMatrix(t15)
+# SHAP
+shap_result <- shap.score.rank(xgb_model = xgb8,
+                               X_train = as.matrix(train_data_xgb[, c(2:3, 6:97)]),
+                               shap_approx = F)
+shap_long <- shap.prep(shap = shap_result,
+                       X_train = as.matrix(train_data_xgb[, c(2:3, 6:97)]),
+                       top_n = 10)
+shap_rp_move_sa <- plot.shap.summary(data_long = shap_long)
+ggsave(shap_rp_move_sa, file = "G:/My Drive/MSBA/Sem1/ML/Final Project/nfl-play-predictor/shap_rp_move_sa.jpeg", dpi = 600)
 
 ############################################ EPA ANALYSIS ############################################
 # Trust The Process ... These ones without movement may not do great
